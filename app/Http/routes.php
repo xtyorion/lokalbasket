b479ceb81878fb5c31e5252ded46d27dd811e921
Vincent <ploweens@gmail.com>
2016-04-03 17:17:47 +0800

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,10 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
+/*  Admin Side Controller
+----------------------------
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(array('domain' => 'admin.lokalbasket.dev'), function()
+{
+Route::get();
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
 });
